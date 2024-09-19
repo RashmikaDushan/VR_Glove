63f4624f9b543c5efe76e19093a7627f35a50996
BALASOORIYA K.B.R.D. <kbrashmikadushan@gmail.com>
2024-09-19 17:52:11 +0530

    local rotation added to fingers

--- UnitySerial/Assets/Scripts/SerialRead.cs
+++ UnitySerial/Assets/Scripts/SerialRead.cs
@@ -5,15 +5,23 @@ using TMPro;
 using System;
 public class TestConnection : MonoBehaviour
 {
-    SerialPort data_stream = new SerialPort("COM3", 19200);
-    private string receivedstring;
-    private int[] intdata;
-    private string[] datas;
-    public TextMeshProUGUI text1;
+    SerialPort data_stream = new SerialPort("COM3", 19200); // Serial port for reading data
+    private string receivedString; // Data received from the serial port
+    private string[] stringData; // Received data as strings
+    private int[] intData; // Recieved data as integers
+    public TextMeshProUGUI onScreenText;
     public GameObject indexFinger;
+    public GameObject middleFinger;
+    public GameObject ringFinger;
+    public GameObject pinkyFinger;
+    public GameObject thumbFinger;
 
-    public float i;
-    private float indexRotOffset = 108.339f;
+    public float index;
+    public float middle;
+    public float ring;
+    public float pinky;
+    public float thumb;
+    private float[] RotOffset = new float[5]{108.339f,0,0,0,0};
 
     void Start()
     {
@@ -26,11 +34,15 @@ public class TestConnection : MonoBehaviour
         {
             try
             {
-                receivedstring = data_stream.ReadLine(); 
-                datas = receivedstring.Split(',');
-                intdata = Array.ConvertAll(datas, int.Parse);
-                text1.SetText(((float)(intdata[0]/45.5111111)).ToString());
-                indexFinger.transform.eulerAngles = new Vector3(0,0,i+indexRotOffset);
+                receivedString = data_stream.ReadLine();
+                stringData = receivedString.Split(',');
+                intData = Array.ConvertAll(stringData, int.Parse);
+                onScreenText.SetText(((float)(intData[0] / 45.5111111)).ToString());
+                indexFinger.transform.localRotation = Quaternion.Euler(0, 0, index + RotOffset[4]);
+                middleFinger.transform.localRotation = Quaternion.Euler(0, 0, middle + RotOffset[4]);
+                ringFinger.transform.localRotation = Quaternion.Euler(0, 0, ring + RotOffset[4]);
+                pinkyFinger.transform.localRotation = Quaternion.Euler(0, 0, pinky + RotOffset[4]);
+                thumbFinger.transform.localRotation = Quaternion.Euler(thumb + RotOffset[4],0,0);
             }
             catch (System.Exception e)
             {
