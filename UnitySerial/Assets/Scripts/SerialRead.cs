821ba5a681a6cd88772091e5eb43fea339f8b9b6
BALASOORIYA K.B.R.D. <kbrashmikadushan@gmail.com>
2024-09-19 18:59:07 +0530

    accurate fingers bending

--- UnitySerial/Assets/Scripts/SerialRead.cs
+++ UnitySerial/Assets/Scripts/SerialRead.cs
@@ -8,20 +8,18 @@ public class TestConnection : MonoBehaviour
     SerialPort data_stream = new SerialPort("COM3", 19200); // Serial port for reading data
     private string receivedString; // Data received from the serial port
     private string[] stringData; // Received data as strings
-    private int[] intData; // Recieved data as integers
-    public TextMeshProUGUI onScreenText;
-    public GameObject indexFinger;
-    public GameObject middleFinger;
-    public GameObject ringFinger;
-    public GameObject pinkyFinger;
-    public GameObject thumbFinger;
-
-    public float index;
-    public float middle;
-    public float ring;
-    public float pinky;
-    public float thumb;
-    private float[] RotOffset = new float[5]{108.339f,0,0,0,0};
+    private int[] intData = new int[5] {0,0,0,0,0}; // Recieved data as integers
+    public TextMeshProUGUI onScreenText; // Text to display necessary data
+    public GameObject[] fingerFirstJoints = new GameObject[5]; // Array of first joints of the fingers
+    public GameObject[] fingerSecondJoints = new GameObject[5]; // Array of second joints of the fingers
+    public GameObject[] fingerThirdJoints = new GameObject[5]; // Array of third joints of the fingers
+    public int index;
+    public int middle;
+    public int ring;
+    public int pinky;
+    public int thumb;
+    private float conversionFactor = 45.5111111f; // Conversion factor to convert the data to degrees
+    private float[] RotationOffsets = new float[5] { 0, 0, 0, 0, 0 };
 
     void Start()
     {
@@ -37,17 +35,50 @@ public class TestConnection : MonoBehaviour
                 receivedString = data_stream.ReadLine();
                 stringData = receivedString.Split(',');
                 intData = Array.ConvertAll(stringData, int.Parse);
-                onScreenText.SetText(((float)(intData[0] / 45.5111111)).ToString());
-                indexFinger.transform.localRotation = Quaternion.Euler(0, 0, index + RotOffset[4]);
-                middleFinger.transform.localRotation = Quaternion.Euler(0, 0, middle + RotOffset[4]);
-                ringFinger.transform.localRotation = Quaternion.Euler(0, 0, ring + RotOffset[4]);
-                pinkyFinger.transform.localRotation = Quaternion.Euler(0, 0, pinky + RotOffset[4]);
-                thumbFinger.transform.localRotation = Quaternion.Euler(thumb + RotOffset[4],0,0);
             }
             catch (System.Exception e)
             {
                 Debug.LogWarning("Error reading from serial port: " + e.Message);
             }
         }
+        onScreenText.SetText(((float)(intData[0] / 45.5111111)).ToString());
+        FingerBend(0, index);
+        FingerBend(1, middle);
+        FingerBend(2, ring);
+        FingerBend(3, pinky);
+        FingerBend(4, thumb);
     }
+
+    void OnApplicationQuit()
+    {
+        data_stream.Close(); // Close the Serial stream
+    }
+
+    void FingerBend(int fingerNumber, int bentPercentage)
+    {
+        if (bentPercentage < 4096 || bentPercentage >= 0)
+        {
+            if (fingerNumber == 4)
+            {
+                fingerFirstJoints[fingerNumber].transform.localRotation = Quaternion.Euler((float)(bentPercentage / conversionFactor) + RotationOffsets[fingerNumber], 0, 0);
+                fingerSecondJoints[fingerNumber].transform.localRotation = Quaternion.Euler((float)(bentPercentage / conversionFactor) + RotationOffsets[fingerNumber], 0, 0);
+                fingerThirdJoints[fingerNumber].transform.localRotation = Quaternion.Euler((float)(bentPercentage / conversionFactor) + RotationOffsets[fingerNumber], 0, 0);
+            }
+            else if (fingerNumber <= 3 || fingerNumber >= 0)
+            {
+                fingerFirstJoints[fingerNumber].transform.localRotation = Quaternion.Euler(0, 0, (float)(bentPercentage / conversionFactor) + RotationOffsets[fingerNumber]);
+                fingerSecondJoints[fingerNumber].transform.localRotation = Quaternion.Euler(0, 0, (float)(bentPercentage / conversionFactor) + RotationOffsets[fingerNumber]);
+                fingerThirdJoints[fingerNumber].transform.localRotation = Quaternion.Euler(0, 0, (float)(bentPercentage / conversionFactor) + RotationOffsets[fingerNumber]);
+            }
+            else
+            {
+                Debug.LogWarning("Invalid finger number");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Invalid bent percentage");
+        }
+    }
+
 }
