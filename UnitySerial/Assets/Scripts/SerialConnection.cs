92de336874453259713ea5672bb2da594b75d1b4
BALASOORIYA K.B.R.D. <kbrashmikadushan@gmail.com>
2024-09-28 15:40:59 +0530

    rotation tracking working correctly

--- UnitySerial/Assets/Scripts/SerialConnection.cs
+++ UnitySerial/Assets/Scripts/SerialConnection.cs
@@ -6,11 +6,13 @@ using System;
 using UnityEngine.XR;
 public class SerialConnection : MonoBehaviour
 {
-    SerialPort serialPort = new SerialPort("COM3", 19200); // Serial port for reading data
+    SerialPort serialPort = new SerialPort("COM3", 115200); // Serial port for reading data
     private string receivedString; // Data received from the serial port
     private string sendString; // Data to be sent to the serial port
     private string[] stringData; // Received data as strings
     private int[] intData = new int[5] { 0, 0, 0, 0, 0 }; // Recieved data as integers
+    private float[] floatData = new float[3] { 0, 0, 0}; // Recieved data as floats
+    private float[] rotationOffset = new float[3] { 0, 0, 0};
     public TextMeshProUGUI onScreenText; // Text to display necessary data
     public GameObject[] fingerFirstJoints = new GameObject[5]; // Array of first joints of the fingers
     public GameObject[] fingerSecondJoints = new GameObject[5]; // Array of second joints of the fingers
@@ -21,8 +23,9 @@ public class SerialConnection : MonoBehaviour
     public int ring;
     public int pinky;
     public int thumb;
+
+    public GameObject hand; // The hand object
     private float conversionFactor = 45.5111111f; // Conversion factor to convert the data to degrees
-    private float[] RotationOffsets = new float[5] { 0, 0, 0, 0, 0 };
 
     void Start()
     {
@@ -40,8 +43,9 @@ public class SerialConnection : MonoBehaviour
             {
                 receivedString = serialPort.ReadLine(); // Read the data from the serial port
                 stringData = receivedString.Split(',');
-                intData = Array.ConvertAll(stringData, int.Parse);
+                floatData = Array.ConvertAll(stringData, float.Parse);
                 serialPort.WriteLine(sendString); // Send the data to the serial port
+                // Debug.Log("Converted floatData: " + string.Join(", ", floatData)); // Debug log the converted floatData
             }
             catch (System.Exception e)
             {
@@ -49,36 +53,37 @@ public class SerialConnection : MonoBehaviour
                 onScreenText.text = "Error reading from serial port.";
             }
         }
+        hand.transform.localEulerAngles = new Vector3(-floatData[2]-rotationOffset[0], floatData[0]-rotationOffset[1], floatData[1]-rotationOffset[2]);
+        // this.transform.localRotation = Quaternion.Euler(intData[0], intData[1], intData[2]);
         FingerBendDebug(0, index);
         FingerBendDebug(1, middle);
         FingerBendDebug(2, ring);
         FingerBendDebug(3, pinky);
         FingerBendDebug(4, thumb);
         // FingerBend();
-        string arrayValues = string.Join(", ", fingerCollided);
-        Debug.Log("boolArray: " + arrayValues);
+        // string arrayValues = string.Join(", ", fingerCollided);
+        // Debug.Log("boolArray: " + arrayValues);
     }
 
     void OnApplicationQuit()
     {
         serialPort.Close(); // Close the Serial stream
     }
-
     void FingerBendDebug(int fingerNumber, int bentPercentage)
     {
         if (bentPercentage < 4096 || bentPercentage >= 0)
         {
             if (fingerNumber == 4)
             {
-                fingerFirstJoints[fingerNumber].transform.localRotation = Quaternion.Euler((float)(bentPercentage / conversionFactor) + RotationOffsets[fingerNumber], 0, 0);
-                fingerSecondJoints[fingerNumber].transform.localRotation = Quaternion.Euler((float)(bentPercentage / conversionFactor) + RotationOffsets[fingerNumber], 0, 0);
-                fingerThirdJoints[fingerNumber].transform.localRotation = Quaternion.Euler((float)(bentPercentage / conversionFactor) + RotationOffsets[fingerNumber], 0, 0);
+                fingerFirstJoints[fingerNumber].transform.localRotation = Quaternion.Euler((float)(bentPercentage / conversionFactor), 0, 0);
+                fingerSecondJoints[fingerNumber].transform.localRotation = Quaternion.Euler((float)(bentPercentage / conversionFactor), 0, 0);
+                fingerThirdJoints[fingerNumber].transform.localRotation = Quaternion.Euler((float)(bentPercentage / conversionFactor), 0, 0);
             }
             else if (fingerNumber <= 3 || fingerNumber >= 0)
             {
-                fingerFirstJoints[fingerNumber].transform.localRotation = Quaternion.Euler(0, 0, (float)(bentPercentage / conversionFactor) + RotationOffsets[fingerNumber]);
-                fingerSecondJoints[fingerNumber].transform.localRotation = Quaternion.Euler(0, 0, (float)(bentPercentage / conversionFactor) + RotationOffsets[fingerNumber]);
-                fingerThirdJoints[fingerNumber].transform.localRotation = Quaternion.Euler(0, 0, (float)(bentPercentage / conversionFactor) + RotationOffsets[fingerNumber]);
+                fingerFirstJoints[fingerNumber].transform.localRotation = Quaternion.Euler(0, 0, (float)(bentPercentage / conversionFactor));
+                fingerSecondJoints[fingerNumber].transform.localRotation = Quaternion.Euler(0, 0, (float)(bentPercentage / conversionFactor));
+                fingerThirdJoints[fingerNumber].transform.localRotation = Quaternion.Euler(0, 0, (float)(bentPercentage / conversionFactor));
             }
             else
             {
@@ -98,15 +103,15 @@ public class SerialConnection : MonoBehaviour
             {
                 if (i == 4)
                 {
-                    fingerFirstJoints[i].transform.localRotation = Quaternion.Euler((float)(intData[i] / conversionFactor) + RotationOffsets[i], 0, 0);
-                    fingerSecondJoints[i].transform.localRotation = Quaternion.Euler((float)(intData[i] / conversionFactor) + RotationOffsets[i], 0, 0);
-                    fingerThirdJoints[i].transform.localRotation = Quaternion.Euler((float)(intData[i] / conversionFactor) + RotationOffsets[i], 0, 0);
+                    fingerFirstJoints[i].transform.localRotation = Quaternion.Euler((float)(intData[i] / conversionFactor), 0, 0);
+                    fingerSecondJoints[i].transform.localRotation = Quaternion.Euler((float)(intData[i] / conversionFactor), 0, 0);
+                    fingerThirdJoints[i].transform.localRotation = Quaternion.Euler((float)(intData[i] / conversionFactor), 0, 0);
                 }
                 else if (i <= 3 || i >= 0)
                 {
-                    fingerFirstJoints[i].transform.localRotation = Quaternion.Euler(0, 0, (float)(intData[i] / conversionFactor) + RotationOffsets[i]);
-                    fingerSecondJoints[i].transform.localRotation = Quaternion.Euler(0, 0, (float)(intData[i] / conversionFactor) + RotationOffsets[i]);
-                    fingerThirdJoints[i].transform.localRotation = Quaternion.Euler(0, 0, (float)(intData[i] / conversionFactor) + RotationOffsets[i]);
+                    fingerFirstJoints[i].transform.localRotation = Quaternion.Euler(0, 0, (float)(intData[i] / conversionFactor));
+                    fingerSecondJoints[i].transform.localRotation = Quaternion.Euler(0, 0, (float)(intData[i] / conversionFactor));
+                    fingerThirdJoints[i].transform.localRotation = Quaternion.Euler(0, 0, (float)(intData[i] / conversionFactor));
                 }
                 else
                 {
@@ -126,5 +131,13 @@ public class SerialConnection : MonoBehaviour
     public void ClearCollision(int fingerNumber)
     {
         fingerCollided[fingerNumber] = false;
+    }
+
+    public void resetTransform()
+    {
+        rotationOffset[0] = -floatData[2];
+        rotationOffset[1] = floatData[0];
+        rotationOffset[2] = floatData[1];
+        Debug.Log("Rotation Euler: " + string.Join(", ", rotationOffset)); // Debug log the converted floatData
     }
 }
