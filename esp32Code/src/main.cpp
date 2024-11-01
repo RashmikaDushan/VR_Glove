203a7cfd8a6e6a982d8491ec18f36fa8765974ac
RashmikaDushan <kbrashmikadushan@gmail.com>
2024-11-01 20:16:27 +0530

    added all three IMUs and all working

--- esp32Code/src/main.cpp
+++ esp32Code/src/main.cpp
@@ -10,13 +10,23 @@
 #define ACTIVATE_IMU 1
 #define ACTIVATE_SERVO 0
 #define CALIBRATE_POTS 0
+#define IMU1_ACTIVED 1 // wire1 = 0x69
+#define IMU2_ACTIVED 1 // wire2 = 0x68
+#define IMU3_ACTIVED 1 // wire1 = 0x68
+#define DEBUG
 
 #if ACTIVATE_IMU
-TwoWire wire = TwoWire(0);
-TwoWire wire1 = TwoWire(1);
-IMU imu(0x68, &wire);
-IMU imu1(0x69, &wire);
-IMU imu2(0x68, &wire1);
+TwoWire wire1 = TwoWire(0);
+TwoWire wire2 = TwoWire(1);
+#if IMU1_ACTIVED
+IMU imu1(0x69, &wire1);
+#endif
+#if IMU2_ACTIVED
+IMU imu2(0x68, &wire2);
+#endif
+#if IMU3_ACTIVED
+IMU imu3(0x68, &wire1);
+#endif
 float *rotation;
 float *rotation1;
 float *rotation2;
@@ -38,11 +48,21 @@ void setup()
 {
   Serial.begin(115200);
 #if ACTIVATE_IMU
-  wire.begin(21, 22, 100000);
-  wire1.begin(16, 17, 100000);
-  imu.begin();
+  wire1.begin(21, 22, 100000);
+  wire2.begin(16, 17, 100000);
+
+#if IMU1_ACTIVED
   imu1.begin();
+#endif
+
+#if IMU2_ACTIVED
   imu2.begin();
+#endif
+
+#if IMU3_ACTIVED
+  imu3.begin();
+#endif
+
 #endif
 
 #if ACTIVATE_POTS && CALLIBRATE_POTS
@@ -58,29 +78,37 @@ void loop()
 {
   Serial.println("");
 #if ACTIVATE_IMU
+
+#if IMU1_ACTIVED
   Serial.print("IMU 01 : ");
-  rotation = imu.returnData(false);
+  rotation = imu1.returnData(false);
   Serial.print(rotation[0]);
   Serial.print(" ");
   Serial.print(rotation[1]);
   Serial.print(" ");
   Serial.print(rotation[2]);
+#endif
 
+#if IMU2_ACTIVED
   Serial.print(" |  IMU 02 : ");
-  rotation1 = imu1.returnData(false);
+  rotation1 = imu2.returnData(false);
   Serial.print(rotation1[0]);
   Serial.print(" ");
   Serial.print(rotation1[1]);
   Serial.print(" ");
   Serial.print(rotation1[2]);
+#endif
 
+#if IMU3_ACTIVED
   Serial.print(" |  IMU 03 : ");
-  rotation2 = imu2.returnData(false);
+  rotation2 = imu3.returnData(false);
   Serial.print(rotation2[0]);
   Serial.print(" ");
   Serial.print(rotation2[1]);
   Serial.print(" ");
   Serial.print(rotation2[2]);
+#endif
+
 #endif
 #if ACTIVATE_POTS
   indexPercent = indexFinger.readValue();
