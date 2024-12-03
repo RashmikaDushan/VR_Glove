1181ec7cb9bd56fb0e8d05e2bb49b48a52674046
BALASOORIYA K.B.R.D. <kbrashmikadushan@gmail.com>
2024-12-03 11:37:28 +0530

    for the final evaluation

--- UnitySerial/Assets/Scripts/SerialConnection.cs
+++ UnitySerial/Assets/Scripts/SerialConnection.cs
@@ -6,25 +6,31 @@ using System;
 using UnityEngine.XR;
 public class SerialConnection : MonoBehaviour
 {
-    SerialPort serialPort = new SerialPort("COM3", 115200); // Serial port for reading data
+    SerialPort serialPort = new SerialPort("COM3", 460800); // Serial port for reading data
     private string receivedString; // Data received from the serial port
     private string sendString; // Data to be sent to the serial port
     private string[] stringData; // Received data as strings
-    private float[] floatData = new float[3] { 0, 0, 0}; // Recieved data as floats yaw,pitch,roll,index,middle,ring,pinky
+    private float[] floatData = new float[14] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // Recieved data as floats yaw,pitch,roll,index,middle,ring,pinky
     private float[] rotationOffset = new float[3] { 0, 0, 0};
+    private float[] rotationOffset1 = new float[3] { 0, 0, 0};
+    private float[] rotationOffset2 = new float[3] { 0, 0, 0};
     public TextMeshProUGUI onScreenText; // Text to display necessary data
     public GameObject[] fingerFirstJoints = new GameObject[5]; // Array of first joints of the fingers
     public GameObject[] fingerSecondJoints = new GameObject[5]; // Array of second joints of the fingers
     public GameObject[] fingerThirdJoints = new GameObject[5]; // Array of third joints of the fingers
-    private bool[] fingerCollided = new bool[5] { false, false, false, false, false }; // Array to check if the fingers are collided
+    private int[] fingerCollided = new int[5] { 0, 0, 0, 0, 0 }; // Array to check if the fingers are collided
+    public int[] fingerCollidedDebug = new int[5] { 0, 0, 0, 0, 0 }; // Array to check if the fingers are collided
     public float index; // variables for debuging
     public float middle;
     public float ring;
     public float pinky;
     public float thumb;
 
-    public GameObject hand; // The hand object
+    public GameObject palm; // The palm object
+    public GameObject forearm; // The forearm object
+    public GameObject arm; // The arm object
     private float conversionFactor = 180.0f; // Conversion factor to convert the data to degrees
+    bool debug=false;
 
     void Start()
     {
@@ -36,16 +42,19 @@ public class SerialConnection : MonoBehaviour
     void Update()
     {
         SerialCommunication();
-        hand.transform.localEulerAngles = new Vector3(-floatData[1]-rotationOffset[0], floatData[0]-rotationOffset[1], -floatData[2]-rotationOffset[2]);
+        palm.transform.localEulerAngles = new Vector3(-floatData[1]-rotationOffset[0], floatData[0]-rotationOffset[1], -floatData[2]-rotationOffset[2]);
+        forearm.transform.localEulerAngles = new Vector3(-floatData[4]-rotationOffset1[0], floatData[3]-rotationOffset1[1], -floatData[5]-rotationOffset1[2]);
+        arm.transform.localEulerAngles = new Vector3(-floatData[7]-rotationOffset2[0], floatData[6]-rotationOffset2[1], -floatData[8]-rotationOffset2[2]);
         // this.transform.localRotation = Quaternion.Euler(intData[0], intData[1], intData[2]);
-        FingerBend(0, index);
-        FingerBend(1, middle);
-        FingerBend(2, ring);
-        FingerBend(3, pinky);
-        FingerBend(4, thumb);
+        FingerBend(0, floatData[12]);
+        FingerBend(1, floatData[11]);
+        FingerBend(2, floatData[10]);
+        FingerBend(3, floatData[9]);
+        // FingerBend(4, floatData[13]);
         // FingerBend();
         // string arrayValues = string.Join(", ", fingerCollided);
         // Debug.Log("boolArray: " + arrayValues);
+        // SendData();
     }
 
     void OnApplicationQuit()
@@ -80,22 +89,35 @@ public class SerialConnection : MonoBehaviour
     }
     public void RecordCollision(int fingerNumber)
     {
-        fingerCollided[fingerNumber] = true;
+        fingerCollided[fingerNumber] = 1;
     }
     public void ClearCollision(int fingerNumber)
     {
-        fingerCollided[fingerNumber] = false;
+        fingerCollided[fingerNumber] = 0;
     }
     public void ResetTransform()
     {
         rotationOffset[0] = -floatData[2];
         rotationOffset[1] = floatData[0];
         rotationOffset[2] = floatData[1];
+
+        rotationOffset1[0] = -floatData[5];
+        rotationOffset1[1] = floatData[3];
+        rotationOffset1[2] = floatData[4];
+
+        rotationOffset2[0] = -floatData[8];
+        rotationOffset2[1] = floatData[6];
+        rotationOffset2[2] = floatData[7];
     }
     private void SerialCommunication(){
-        sendString = string.Join(", ", fingerCollided); // prepare the data to be sent to the serial port
-
-        if (serialPort.IsOpen)
+        if(debug){
+            sendString = string.Join(", ", fingerCollidedDebug); // prepare the data to be sent to the serial port
+        }
+        else{
+            sendString = string.Join(", ", fingerCollided); // prepare the data to be sent to the serial port
+        }
+        
+        if (serialPort.IsOpen && serialPort.BytesToRead > 0)
         {
             try
             {
@@ -104,20 +126,39 @@ public class SerialConnection : MonoBehaviour
                 floatData = Array.ConvertAll(stringData, float.Parse);
                 serialPort.WriteLine(sendString); // Send the data to the serial port
                 Debug.Log("Converted floatData: " + string.Join(", ", floatData)); // Debug log the converted floatData
+                if(debug){
+                    Debug.Log("FingerCollided Debug: " + sendString);
+                }
+                else{
+                    Debug.Log("FingerCollided: " + sendString);
+                }
+                onScreenText.text = "FingerCollided: " + sendString;
+                // Debug.Log("Received: " + receivedString);
             }
             catch (System.Exception e)
             {
                 Debug.LogWarning("Error reading from serial port: " + e.Message);
-                onScreenText.text = "Error reading from serial port.";
+                // onScreenText.text = "Error reading from serial port.";
             }
         }
     }
 
-        public void resetTransform()
-    {
-        rotationOffset[0] = -floatData[2];
-        rotationOffset[1] = floatData[0];
-        rotationOffset[2] = floatData[1];
-        Debug.Log("Rotation Euler: " + string.Join(", ", rotationOffset)); // Debug log the converted floatData
-    }
+    //     public void resetTransform()
+    // {
+    //     rotationOffset[0] = -floatData[2];
+    //     rotationOffset[1] = floatData[0];
+    //     rotationOffset[2] = floatData[1];
+    //     Debug.Log("Rotation Euler: " + string.Join(", ", rotationOffset)); // Debug log the converted floatData
+    // }
+
+    // void SendData()
+    // {
+    //     if (serialPort != null && serialPort.IsOpen)
+    //     {
+    //         string data = $"{fingerCollided[0]},{fingerCollided[1]},{fingerCollided[2]},{fingerCollided[3]},{fingerCollided[4]}\n";
+    //         serialPort.WriteLine(data); // Send the data string
+    //         Debug.Log($"Sent: {data}");
+    //     }
+    // }
 }
+
--- esp32Code/src/main.cpp
+++ esp32Code/src/main.cpp
@@ -6,14 +6,16 @@
 #include <IO.h>
 #include "Wire.h"
 
-#define ACTIVATE_POTS 0
+// used pins - (Wire 1) 21, 22, (Wire 2) 16, 17, (Pots) 33, 32, 35, 34, (servo1) 18 , (servo2) 19, (servo3) 2 , (servo4) 4
+
+#define ACTIVATE_POTS 1
 #define ACTIVATE_IMU 1
-#define ACTIVATE_SERVO 0
+#define ACTIVATE_SERVO 1
 #define CALIBRATE_POTS 0
 #define IMU1_ACTIVED 1 // wire1 = 0x69
 #define IMU2_ACTIVED 1 // wire2 = 0x68
-#define IMU3_ACTIVED 1 // wire1 = 0x68
-#define DEBUG
+#define IMU3_ACTIVED 0 // wire1 = 0x68
+#define DEBUG 0
 
 #if ACTIVATE_IMU
 TwoWire wire1 = TwoWire(0);
@@ -32,6 +34,9 @@ float *rotation1;
 float *rotation2;
 #endif
 
+int closeAngle = 0;
+float angleRatio = 45.0;
+
 #if ACTIVATE_POTS
 Potentiometer indexFinger(33, "index", CALIBRATE_POTS);
 Potentiometer middleFinger(32, "middle", CALIBRATE_POTS);
@@ -41,12 +46,19 @@ float indexPercent, middlePercent, ringPercent, pinkyPercent;
 #endif
 
 #if ACTIVATE_SERVO
-ServoMotor servo(18);
+ServoMotor servo1(18); // index
+ServoMotor servo2(19); // middle
+ServoMotor servo3(2); // ring
+ServoMotor servo4(4); // pinky
+// ServoMotor servo5(18);
+int collided[5] = {0, 0, 0, 0, 0};
+bool holdRestriction[5] = {0, 0, 0, 0, 0};
 #endif
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(460800);
+  setCpuFrequencyMhz(240);
 #if ACTIVATE_IMU
   wire1.begin(21, 22, 100000);
   wire2.begin(16, 17, 100000);
@@ -78,7 +90,7 @@ void loop()
 {
   Serial.println("");
 #if ACTIVATE_IMU
-
+#if DEBUG
 #if IMU1_ACTIVED
   Serial.print("IMU 01 : ");
   rotation = imu1.returnData(false);
@@ -108,13 +120,64 @@ void loop()
   Serial.print(" ");
   Serial.print(rotation2[2]);
 #endif
+#endif
+#if !DEBUG
+#if IMU1_ACTIVED
+  rotation = imu1.returnData(false);
+  Serial.print(rotation[0]);
+  Serial.print(",");
+  Serial.print(rotation[1]);
+  Serial.print(",");
+  Serial.print(rotation[2]);
+#else 
+  Serial.print(",");
+  Serial.print("0.0");
+  Serial.print(",");
+  Serial.print("0.0");
+  Serial.print(",");
+  Serial.print("0.0");
+#endif
 
+#if IMU2_ACTIVED
+  rotation1 = imu2.returnData(false);
+  Serial.print(",");
+  Serial.print(rotation1[0]);
+  Serial.print(",");
+  Serial.print(rotation1[1]);
+  Serial.print(",");
+  Serial.print(rotation1[2]);
+#else 
+  Serial.print(",");
+  Serial.print("0.0");
+  Serial.print(",");
+  Serial.print("0.0");
+  Serial.print(",");
+  Serial.print("0.0");
+#endif
+
+#if IMU3_ACTIVED
+  rotation2 = imu3.returnData(false);
+  Serial.print(",");
+  Serial.print(rotation2[0]);
+  Serial.print(",");
+  Serial.print(rotation2[1]);
+  Serial.print(",");
+  Serial.print(rotation2[2]);
+#else 
+  Serial.print(",");
+  Serial.print("0.0");
+  Serial.print(",");
+  Serial.print("0.0");
+  Serial.print(",");
+  Serial.print("0.0");
+#endif
+#endif
 #endif
 #if ACTIVATE_POTS
   indexPercent = indexFinger.readValue();
   middlePercent = middleFinger.readValue();
   ringPercent = ringFinger.readValue();
-  pinkyPercent = pinkyFinger.readValue();
+  pinkyPercent = pinkyFinger.readValue()-0.27;
   Serial.print(",");
   Serial.print(indexPercent);
   Serial.print(",");
@@ -124,9 +187,63 @@ void loop()
   Serial.print(",");
   Serial.print(pinkyPercent);
 #endif
-  // int angle = map(potPercent, 0, 4095, 0, 180);
-  // angle = 180 - angle;
-  // servo.writeAngle(angle);
-  delay(10);
-  delay(10);
+delay(1);
+#if ACTIVATE_SERVO
+    if (Serial.available() > 0) {
+        String input = Serial.readStringUntil('\n');
+        int index = 0;
+        while (input.length() > 0 && index < 5) {
+            int commaIndex = input.indexOf(',');
+            if (commaIndex == -1) {
+                collided[index++] = input.toInt();
+                break;
+            } else {
+                collided[index++] = input.substring(0, commaIndex).toInt();
+                input = input.substring(commaIndex + 1);
+            }
+        }
+
+      #if ACTIVATE_POTS
+        if (index >= 5) {
+          if (collided[0] == 1 and holdRestriction[0] == 0) {
+            servo1.writeAngle(indexPercent*angleRatio);
+            holdRestriction[0] = 1;
+          }
+          else if(collided[0] == 0 ){
+            servo1.writeAngle(closeAngle);
+            holdRestriction[0] = 0;
+          }
+                    if (collided[1] == 1 and holdRestriction[1] == 0) {
+            servo1.writeAngle(indexPercent*angleRatio);
+            holdRestriction[1] = 1;
+          }
+          else if(collided[1] == 0 ){
+            servo1.writeAngle(closeAngle);
+            holdRestriction[1] = 0;
+          }
+                    if (collided[2] == 1 and holdRestriction[2] == 0) {
+            servo1.writeAngle(indexPercent*angleRatio);
+            holdRestriction[2] = 1;
+          }
+          else if(collided[2] == 0 ){
+            servo1.writeAngle(closeAngle);
+            holdRestriction[2] = 0;
+          }
+                    if (collided[3] == 1 and holdRestriction[3] == 0) {
+            servo1.writeAngle(indexPercent*angleRatio);
+            holdRestriction[3] = 1;
+          }
+          else if(collided[3] == 0 ){
+            servo1.writeAngle(closeAngle);
+            holdRestriction[3] = 0;
+          }
+          // servo1.writeAngle(indexPercent*angleRatio);
+          // if (collided[4] != 0) {
+          //   servo5.writeAngle();
+          // }
+        }
+    #endif
+    }
+  #endif
+  delay(1);
 }
